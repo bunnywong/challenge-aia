32fe34c49328b511723e2637899aed6cdc8e229c
BunnY <w@bunnywong.com>
2021-02-07 09:32:46 +0800

    component table

---
+++ src/components/molecules/hero-table/index.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
+import xor from 'lodash/xor'
+
+function HeroTable({ items }) {
+  const [favorites, setFavorites] = useState([])
+
+  const handleClickFavorite = (id) => {
+    const favoriteIds = JSON.parse(localStorage.getItem('favoriteIds'))
+    const result = xor(favoriteIds, [id])
+    localStorage.setItem('favoriteIds', JSON.stringify(result))
+    setFavorites(result)
+  }
+  const getFavorite = (cell, row) => {
+    return (
+      <button onClick={() => handleClickFavorite(row.id)}>
+        {favorites.includes(row.id) ? '❤️' : '🖤'}
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    setFavorites(JSON.parse(localStorage.getItem('favoriteIds')))
+  }, [])
+
+  return (
+    <BootstrapTable data={items} striped hover condensed search>
+      <TableHeaderColumn dataField="favorite" width="100" dataFormat={getFavorite} dataSort>
+        Favorite
+      </TableHeaderColumn>
+      <TableHeaderColumn dataField="id" width="100" isKey dataSort>
+        ID
+      </TableHeaderColumn>
+      <TableHeaderColumn dataField="name" dataSort>
+        Name
+      </TableHeaderColumn>
+    </BootstrapTable>
+  )
+}
+
+export default HeroTable
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Switch from 'react-switch'
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table'
-import xor from 'lodash/xor'
 
 import Layout from 'src/global/Layout'
 import axios from 'src/components/_settings/axios'
 import HeroCarousel from 'src/components/molecules/hero-carousel/'
+import HeroTable from 'src/components/molecules/hero-table/'
 
 function IndexPage() {
   const [items, setItems] = useState([])
-  const [favorites, setFavorites] = useState([])
   const [random, setRandom] = useState(false)
   const imagesCount = 8
   const itemRangeRandom = Array.from(
@@ -23,7 +21,6 @@ function IndexPage() {
   }
   const itemRange = random ? itemRangeRandom : [1, 2, 3, 4, 5, 6, 7, 8]
   const fetchList = async () => {
-    const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
     const fetchItem = async (id) => {
       await axios.get(`/${id}`).then((response) =>
         setItems((state) => {
@@ -31,26 +28,14 @@ function IndexPage() {
         })
       )
     }
+    const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
     for (let i = 0; i < imagesCount; i++) {
       fetchItem(itemRange[i])
       await sleep(process.env.FETCH_SLEEP)
     }
   }
-  const handleClickFavorite = (id) => {
-    const favoriteIds = JSON.parse(localStorage.getItem('favoriteIds'))
-    const result = xor(favoriteIds, [id])
-    localStorage.setItem('favoriteIds', JSON.stringify(result))
-    setFavorites(result)
-  }
-  const getFavorite = (cell, row) => {
-    return (
-      <button onClick={() => handleClickFavorite(row.id)}>
-        {favorites.includes(row.id) ? '❤️' : '🖤'}
-      </button>
-    )
-  }
+
   useEffect(() => {
-    setFavorites(JSON.parse(localStorage.getItem('favoriteIds')))
     if (localStorage.getItem('randomImages') === 'true') {
       setRandom(true)
     }
@@ -69,17 +54,7 @@ function IndexPage() {
         </div>
       </section>
       <section>
-        <BootstrapTable data={items} striped hover condensed search>
-          <TableHeaderColumn dataField="favorite" width="100" dataFormat={getFavorite} dataSort>
-            Favorite
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="id" width="100" isKey dataSort>
-            ID
-          </TableHeaderColumn>
-          <TableHeaderColumn dataField="name" dataSort>
-            Name
-          </TableHeaderColumn>
-        </BootstrapTable>
+        <HeroTable items={items} />
       </section>
     </Layout>
   )
