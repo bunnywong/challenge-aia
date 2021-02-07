8eb6e532c28f2dd850f59e49d58a29c79b91f6ba
BunnY <w@bunnywong.com>
2021-02-07 11:26:36 +0800

    tweak

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { configure, addDecorator } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import styled from 'styled-components';
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import styled from 'styled-components'
 import GlobalStyle from '../src/components/_settings/GlobalStyle'
 
 const Wrapper = styled.div`
@@ -10,31 +10,31 @@ const Wrapper = styled.div`
   height: 100vh;
   align-items: center;
   justify-content: center;
-`;
+`
 
-const Decorator = storyFn => (
+const Decorator = (storyFn) => (
   <Wrapper>
     <GlobalStyle />
     {storyFn()}
   </Wrapper>
-);
+)
 
-addDecorator(Decorator);
+addDecorator(Decorator)
 
 global.___loader = {
-  enqueue: () => { },
-  hovering: () => { },
-};
-
-global.__PATH_PREFIX__ = '';
-window.___navigate = (pathname) => {
-  action('NavigateTo:')(pathname);
-};
-
-const req = require.context('../src/components', true, /\.stories\.js$/);
-
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  enqueue: () => {},
+  hovering: () => {},
 }
 
-configure(loadStories, module);
+global.__PATH_PREFIX__ = ''
+window.___navigate = (pathname) => {
+  action('NavigateTo:')(pathname)
+}
+
+const req = require.context('../src/components', true, /\.stories\.js$/)
+
+function loadStories() {
+  req.keys().forEach((filename) => req(filename))
+}
+
+configure(loadStories, module)
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 import Layout from 'src/components/templates/Layout'
 
@@ -7,6 +7,6 @@ const NotFoundPage = () => (
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
-);
+)
 
-export default NotFoundPage;
+export default NotFoundPage
